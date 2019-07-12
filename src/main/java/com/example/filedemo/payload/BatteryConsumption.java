9726b738f838f83f5195b029f324cce46b7cbdd1
Marc Poris <marc.poris@gmail.com>
2019-07-12 10:42:33 -0700

    Calculate percentages.

--- src/main/java/com/example/filedemo/payload/BatteryConsumption.java
+++ src/main/java/com/example/filedemo/payload/BatteryConsumption.java
@@ -7,10 +7,20 @@ public class BatteryConsumption {
     private long fullChargeVolume;
     private long startingVolume;
     private long endingVolume;
+    private long startingVolumePercentage;
+    private long endingVolumePercentage;
     
     public BatteryConsumption(long fullChargeVolume, long startingVolume, long endingVolume) {
+        this.fullChargeVolume = fullChargeVolume;
         this.startingVolume = startingVolume;
         this.endingVolume = endingVolume;
+        
+        computePercentages();
+    }
+
+    private void computePercentages() {
+        this.startingVolumePercentage = this.startingVolume * 100 / this.fullChargeVolume;
+        this.endingVolumePercentage = this.endingVolume * 100 / this.fullChargeVolume;
     }
 
     public long getFullChargeVolume() {
@@ -23,6 +33,14 @@ public class BatteryConsumption {
 
     public long getEndingVolume() {
         return this.endingVolume;
+    }
+
+    public long getStartingVolumePercentage() {
+        return this.startingVolumePercentage;
+    }
+
+    public long getEndingVolumePercentage() {
+        return this.endingVolumePercentage;
     }
 
     @Override
