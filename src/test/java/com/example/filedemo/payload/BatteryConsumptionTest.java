bde379cba8c8063a128ae895de99abdb6d9a1d6b
Marc Poris <marc.poris@gmail.com>
2019-07-12 11:19:15 -0700

    Test sad paths for BatteryConsumption.

--- src/test/java/com/example/filedemo/payload/BatteryConsumptionTest.java
+++ src/test/java/com/example/filedemo/payload/BatteryConsumptionTest.java
@@ -14,4 +14,20 @@ public class BatteryConsumptionTest {
 		assertEquals(95, batteryConsumption.getStartingVolumePercentage());
 		assertEquals(75, batteryConsumption.getEndingVolumePercentage());
 	}
+
+	@Test
+	public void computePercentages_zeroFullCharge() throws Exception {
+		BatteryConsumption batteryConsumption = new BatteryConsumption(0, 950, 750);
+
+		assertEquals(0, batteryConsumption.getStartingVolumePercentage());
+		assertEquals(0, batteryConsumption.getEndingVolumePercentage());
+	}
+
+	@Test
+	public void computePercentages_negativeFullCharge() throws Exception {
+		BatteryConsumption batteryConsumption = new BatteryConsumption(-1000, 950, 750);
+
+		assertEquals(0, batteryConsumption.getStartingVolumePercentage());
+		assertEquals(0, batteryConsumption.getEndingVolumePercentage());
+	}
 }
